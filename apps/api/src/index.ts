1e4cd0090bb4bb1713e6b87c41d6b86cbd2e6b8d
libresoul <contact@damindu.me>
2026-08-03 00:04:48 +0530

    chore(api): add logger

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,35 @@
 import express, { Request, Response } from 'express';
+import pino from 'pino'
+import { pinoHttp } from 'pino-http';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const logger = pino({
+  level: 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      levelFirst: true,
+      translateTime: 'HH:MM:ss'
+    }
+  },
+})
 
+app.use(pinoHttp({
+  logger, serializers: {
+    req: () => undefined,
+    res: () => undefined,
+    responseTime: () => undefined
+  },
+  customProps: (req, res) => {
+    return {
+      method: req.method,
+      path: req.path,
+      statusCode: res.statusCode,
+    }
+  }
+}))
 app.use(express.json());
 
 app.get('/', (_: Request, res: Response) => {
@@ -10,5 +37,5 @@ app.get('/', (_: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  logger.info(`Server is running on http://localhost:${PORT}`);
 });
