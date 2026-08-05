e369b67de38bf899247c7df7a856bf02fabe0ca6
Kasundi Ranaweera <kasundiranaweera@gmail.com>
2026-08-06 01:00:21 +0530

    Add three columns and Create Task button

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,8 +1,14 @@
+import TaskBoard from './components/TaskBoard'
+
 function App() {
   return (
-    <main className="flex m-2 items-center gap-1">
-      <div className="animate-pulse w-3 h-3 rounded-full bg-cyan-400"></div>
-      <h1 className="text-xl font-bold">Syncboard</h1>
+    <main className="p-4">
+      <header className="flex items-center gap-2 mb-4">
+        <div className="animate-pulse w-3 h-3 rounded-full bg-cyan-400"></div>
+        <h1 className="text-xl font-bold">Syncboard</h1>
+      </header>
+
+      <TaskBoard />
     </main>
   )
 }
