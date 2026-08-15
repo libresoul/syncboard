0782e9a005b3a87562de135ecf02a61e89f310cd
libresoul <contact@damindu.me>
2026-08-16 01:39:39 +0530

    refactor: use task modal and properly seed miragejs in-mem db

--- apps/web/src/mockServer.ts
+++ apps/web/src/mockServer.ts
@@ -1,19 +1,37 @@
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
+import type { AnyFactories, Registry } from 'miragejs/-types'
+import type Schema from 'miragejs/orm/schema'
 import mockData from './data/mockTasks'
+import type { Task as TaskShape } from './types/task'
 
 type Environment = {
   environment: 'development' | 'test'
 }
 
+const models = {
+  task: Model.extend<TaskShape>({} as TaskShape)
+}
+
+type AppRegistry = Registry<typeof models, AnyFactories>
+type AppSchema = Schema<AppRegistry>
+
 export function makeServer({ environment = 'development' }: Environment) {
-  const server = createServer({
+  const server = createServer<typeof models, AnyFactories>({
     environment,
+    models,
+
     routes() {
       this.namespace = 'api'
       this.urlPrefix = import.meta.env.VITE_API_URL || 'http://localhost:3000'
 
-      this.get('/tasks', () => {
-        return { tasks: mockData }
+      this.get('/tasks', (schema: AppSchema) => {
+        return schema.all('task')
+      })
+    },
+
+    seeds(server) {
+      mockData.forEach((data) => {
+        server.create('task', data)
       })
     }
   })
