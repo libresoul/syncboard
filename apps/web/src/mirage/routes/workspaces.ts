2b66b99e53a029afcf24c21d9c6e3bff53133e3b
libresoul <contact@damindu.me>
2026-08-23 03:11:40 +0530

    feat: implement /workspace mock api endpoint

---
+++ apps/web/src/mirage/routes/workspaces.ts
@@ -0,0 +1,11 @@
+import type { Server } from 'miragejs/server'
+import type { AppSchema } from '../server'
+
+export default function workspacesRoutes(server: Server) {
+  server.get('/workspaces')
+
+  server.post('/workspaces', (schema: AppSchema, request) => {
+    const attrs = JSON.parse(request.requestBody)
+    return schema.create('workspace', attrs)
+  })
+}
