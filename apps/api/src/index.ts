871a787c7b9352f8f9225fff6538610d7c794fda
libresoul <contact@damindu.me>
2026-08-28 19:41:57 +0530

    chore(api): move pinohttp to middleware/

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,28 +1,12 @@
 import express, { type Request, type Response } from 'express'
-import { pinoHttp } from 'pino-http'
+import { requestLogger } from './middleware/requestLogger'
 import logger from './utils/logger'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req: () => undefined,
-      res: () => undefined,
-      responseTime: () => undefined
-    },
-    customProps: (req, res) => {
-      return {
-        method: req.method,
-        path: req.path,
-        statusCode: res.statusCode
-      }
-    }
-  })
-)
 app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (_: Request, res: Response) => {
   res.json({ message: 'Hello Syncboard' })
