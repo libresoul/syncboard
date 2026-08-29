54bdf8eae378c11b345004827274aa2a53ed9df6
Damindu Dhananjitha <contact@damindu.me>
2026-08-30 03:34:26 +0530

    chore: switch to zod-mini in shared

--- packages/shared/src/schemas/task.schema.ts
+++ packages/shared/src/schemas/task.schema.ts
@@ -1,14 +1,14 @@
-import z from 'zod'
+import * as z from 'zod/mini'
 
 export const taskSchema = z.object({
   id: z.string(),
   title: z.string(),
   status: z.enum(['todo', 'inprogress', 'done']),
-  description: z.string().optional(),
-  tag: z.string().optional(),
-  comments: z.number().optional(),
-  attachments: z.number().optional(),
-  assignee: z.string()
+  description: z.optional(z.string()),
+  tag: z.optional(z.string()),
+  comments: z.optional(z.number()),
+  attachments: z.optional(z.number()),
+  assignee: z.optional(z.string())
 })
 
 export type Task = z.infer<typeof taskSchema>
--- packages/shared/src/schemas/workspace.schema.ts
+++ packages/shared/src/schemas/workspace.schema.ts
@@ -1,10 +1,10 @@
-import { z } from 'zod'
+import * as z from 'zod/mini'
 
 export const workspaceSchema = z.object({
   id: z.string(),
   name: z.string(),
-  description: z.string().optional(),
-  color: z.string().optional()
+  description: z.optional(z.string()),
+  color: z.optional(z.string())
 })
 
 export type Workspace = z.infer<typeof workspaceSchema>
